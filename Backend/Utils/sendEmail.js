1df6e9624b9ca7fdd1c59363c40758df52cbef97
Siddharth Agrawal <07mayhand@gmail.com>
2021-10-17 01:04:35 +0530

    mailing service works fine

--- Backend/Utils/sendEmail.js
+++ Backend/Utils/sendEmail.js
@@ -2,6 +2,7 @@ const nodeMailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   const transporter = nodeMailer.createTransport({
+    //   You can add host and port of service provider for better compatibility
     service: process.env.SMTP_SERVICE,
     auth: {
       user: process.env.SMTP_MAIL,
