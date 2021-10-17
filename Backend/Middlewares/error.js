e457fffe3cfef53f292fb8a8fdd1c41b55b42aa2
Siddharth Agrawal <07mayhand@gmail.com>
2021-10-17 17:26:54 +0530

    error handling

--- Backend/Middlewares/error.js
+++ Backend/Middlewares/error.js
@@ -14,6 +14,18 @@ module.exports = (err, req, res, next) => {
     const message = `Duplicate ${Object.keys(err.keyValue)} entered `;
     err = new ErrorHandler(message, 400);
   }
+  // Wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = "Json Web Token is invalid, try again";
+    err = new ErrorHandler(message, 400);
+  }
+
+  // JWT expire error
+  if (err.name === "TokenExpiredError") {
+    const message = "Json Web Token is expired, try again";
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
