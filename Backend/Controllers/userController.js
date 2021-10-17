d4b22d2a5d097dd019f5f69ecfedec55da0528cc
Siddharth Agrawal <07mayhand@gmail.com>
2021-10-17 17:37:58 +0530

    user details route added

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -121,3 +121,12 @@ exports.resetPassword = catchAsyncError(async (req, res, next) => {
 
   sendToken(user, res, 200);
 });
+
+exports.getUserDetails = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
