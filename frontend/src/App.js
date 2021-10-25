95cf85d683bba2a7cd57a24bbfc5aa218017cee5
Siddharth Agrawal <07mayhand@gmail.com>
2021-10-25 18:00:30 +0530

    navbar , header +++

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import Header from "./component/layout/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
+import WebFont from "webfontloader";
 
 function App() {
-  return <div className="App"></div>;
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto"],
+      },
+    });
+  }, []);
+
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/component/layout/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { ReactNavbar } from "overlay-navbar";
+import logo from "../../images/myStore.png";
+
+function Header() {
+  return (
+    <div>
+      <ReactNavbar
+        burgerColorHover="Grey"
+        logo={logo}
+        navColor1="white"
+        navColor2="white"
+        navColor3="rgb(35, 35, 35)"
+        navColor4="rgb(35, 35, 35)"
+        link1Text="Home"
+        link1Url="/"
+        link2Text="Product"
+        link2Url="/product"
+        link3Text="Contact"
+        link3Url="/contact"
+        link4Text="About"
+        link4Url="/about"
+        link1Size="1.5vmax"
+        link1Margin="0 auto"
+        link3Color="white"
+        link1AnimationTime="0.4"
+        searchIconMargin="1vmax"
+        cartIconMargin="2vmax"
+        profileIconMargin="1vmax"
+      />
+    </div>
+  );
+}
+
+export default Header;
