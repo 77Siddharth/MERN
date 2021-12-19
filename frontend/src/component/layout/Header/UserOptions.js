4023c0fd417068e2efa7a69223d921b8d70adac1
Siddharth Agrawal <07mayhand@gmail.com>
2021-12-19 13:33:17 +0530

    UI minor fixes

--- frontend/src/component/layout/Header/UserOptions.js
+++ frontend/src/component/layout/Header/UserOptions.js
@@ -3,6 +3,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { Fragment } from "react";
 import { SpeedDial, SpeedDialAction } from "@mui/material";
 import { useState } from "react";
+import Backdrop from "@material-ui/core/Backdrop";
 import DashboardIcon from "@material-ui/icons/Dashboard";
 import PersonIcon from "@material-ui/icons/Person";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
@@ -11,8 +12,10 @@ import { useHistory } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import { useDispatch } from "react-redux";
 import { logout } from "../../../actions/userAction";
+import "./Header.css";
+
 function UserOptions({ user }) {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const history = useHistory();
   const dispatch = useDispatch();
   const dashboard = () => {
@@ -46,15 +49,20 @@ function UserOptions({ user }) {
 
   return (
     <Fragment>
+      <Backdrop open={open} style={{ zIndex: 10 }} />
       <SpeedDial
         ariaLabel="SpeedDial tooltip example"
+        className="speedDial"
         onClose={() => setOpen(false)}
         onOpen={() => setOpen(true)}
         icon={<PersonIcon />}
         direction="down"
+        style={{ zIndex: 11 }}
       >
         {options.map((option) => (
           <SpeedDialAction
+            className="speedDialIcon"
+            key={option.name}
             icon={option.icon}
             tooltipTitle={option.name}
             onClick={option.func}
